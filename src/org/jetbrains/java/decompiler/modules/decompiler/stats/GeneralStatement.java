c980f6140f143791e8ec52c0f18b101649f23dbe
LexManos <LexManos@gmail.com>
2017-04-12 10:08:10 -0700

    Fix output discrepancies based on running JVM

--- src/org/jetbrains/java/decompiler/modules/decompiler/stats/GeneralStatement.java
+++ src/org/jetbrains/java/decompiler/modules/decompiler/stats/GeneralStatement.java
@@ -7,7 +7,8 @@ import org.jetbrains.java.decompiler.util.TextBuffer;
 import org.jetbrains.java.decompiler.main.collectors.BytecodeMappingTracer;
 
 import java.util.Collection;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 
 public class GeneralStatement extends Statement {
@@ -27,7 +28,7 @@ public class GeneralStatement extends Statement {
     first = head;
     stats.addWithKey(head, head.id);
 
-    HashSet<Statement> set = new HashSet<>(statements);
+    Set<Statement> set = new LinkedHashSet<>(statements);
     set.remove(head);
 
     for (Statement st : set) {
